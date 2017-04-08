3f2afae92eaf018828c2b285315c2d6d849b3897
bulbule <anyagoremykina@gmail.com>
2017-04-09 00:17:52 +0200

    initial commit

--- format_price.py
+++ format_price.py
@@ -1,5 +1,36 @@
+import argparse
+from decimal import *
+
+
 def format_price(price):
-    pass
+    not_available = 'N/A'
+    try:
+        if not isinstance(price, bool):
+            price = float(price)
+        else:
+            return not_available
+    except (ValueError, TypeError):
+        return not_available
+    if not price:
+        return not_available
+    if (price).is_integer():
+        price = int(price)
+    else:
+        price = round_float_number_to_two_decimals(price)
+    return "{:,}".format(price).replace(',', ' ')
+
+
+def round_float_number_to_two_decimals(number):
+    getcontext().prec = 30
+    return float(
+        Decimal(
+            '{}'.format(number)).quantize(
+            Decimal('.01'),
+            rounding=ROUND_UP))
+
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('price', help='a price to format')
+    args = parser.args_parse()
+    print(format_price(args.price))
--- tests.py
+++ tests.py
@@ -1,1 +1,35 @@
-# TODO place your tests here
+import unittest
+
+from format_price import format_price
+
+class FormatPriceTestCase(unittest.TestCase):
+    def test_big_integer_price_with_zero_decimals(self):
+        self.assertEqual(format_price('123456.000'), '123 456')
+    
+    def test_big_integer_price(self):
+        self.assertEqual(format_price(987654321), '987 654 321')   
+    
+    def test_float_price(self):
+        self.assertEqual(format_price('10.9876'),'10.99')
+        
+    def test_float_price_with_many_decimals(self):
+        self.assertEqual(format_price(0.09876000), '0.1')
+        
+    def test_zero_str_price(self):
+        self.assertEqual(format_price('0'),'N/A')
+        
+    def test_zero_int_price(self):
+        self.assertEqual(format_price(0), 'N/A')
+    
+    def test_garbage_price(self):
+        self.assertEqual(format_price('smth'),'N/A')
+        
+    def test_bool_price(self):
+        self.assertEqual(format_price(True), 'N/A')
+    
+    def test_None_price(self):
+        self.assertEqual(format_price(None), 'N/A')
+        
+    
+if __name__ == '__main__':
+    unittest.main()
